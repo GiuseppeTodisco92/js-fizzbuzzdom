a851a197fb3e5942c84d27f93f928c81d00de5e3
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-02 17:00:48 +0100

    add main complete

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ for( let i = 1; i <= 100; i++ ) {
     const element = document.createElement("li");
 
     element.classList.add("box_number");
-    if (i % 3 == 0){
+    if (i % 3 == 0 && i % 5 == 0){
+        element.innerText = "FizzBuzz";
+        element.classList.add("fizz_buzz");
+    } 
+   else if (i % 3 == 0){
         element.innerText = "Fizz";
+        element.classList.add("fizz");
     } else if (i % 5 == 0) {
         element.innerText = "Buzz";
+        element.classList.add("buzz");
     } else {
         element.innerText = i;
     }
