19fc919cb38663db2c9a69fea6a5127f238e699f
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-02 17:24:24 +0100

    add stamp in consololog

--- js/main.js
+++ js/main.js
@@ -7,16 +7,21 @@ for( let i = 1; i <= 100; i++ ) {
     if (i % 3 == 0 && i % 5 == 0){
         element.innerText = "FizzBuzz";
         element.classList.add("fizz_buzz");
+        console.log(element.innerText);
     } 
    else if (i % 3 == 0){
         element.innerText = "Fizz";
         element.classList.add("fizz");
+        console.log(element.innerText)
     } else if (i % 5 == 0) {
         element.innerText = "Buzz";
         element.classList.add("buzz");
+        console.log(element.innerText)
     } else {
         element.innerText = i;
+        console.log(i);
     }
-   
     list.append(element);
+    
+    
 }
